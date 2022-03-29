65d0aceb502978cd81fc53410615bf080a9f427b
Stefano Guerrini <sguerrini@surveymonkey.com>
2022-03-29 16:03:52 +0200

    Remove health-indicator-kafka-java as dependency
    Start using the open source project https://github.com/deviceinsight/kafka-health-check
    The expected result is the same.

--- kahpp-spring-autoconfigure/src/main/java/dev/vox/platform/kahpp/Application.java
+++ kahpp-spring-autoconfigure/src/main/java/dev/vox/platform/kahpp/Application.java
@@ -1,12 +1,9 @@
 package dev.vox.platform.kahpp;
 
-import com.usabilla.healthcheck.springboot.kafka.KafkaStreamsHealthIndicator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(KafkaStreamsHealthIndicator.class)
 public class Application {
 
   public static void main(String[] args) {
